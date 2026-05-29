e0dbbaf628f133bac3bfd798220d34be931e3a8a
Andreas <zimaos.mule640@passmail.net>
2026-05-29 09:15:27 +0200

    feat: ACME/Let's Encrypt automatic TLS certificate provisioning

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,6 +30,7 @@ import (
 
 	"golang.org/x/time/rate"
 
+	scutumacme "scutum/cmd/internal/acme"
 	"scutum/cmd/internal/auth"
 	"scutum/cmd/internal/handlers"
 	"scutum/cmd/internal/kms"
@@ -619,7 +620,27 @@ func main() {
 		TLSConfig:    tlsConfig,
 	}
 
-	if useTLS {
+	acmeCfg := scutumacme.FromEnv(secretsDir)
+	if acmeCfg.Enabled() {
+		acmeMgr := scutumacme.New(acmeCfg)
+		server.TLSConfig = acmeMgr.TLSConfig()
+		go func() {
+			logger.Info("ACME HTTP-01 challenge server starting", "addr", ":80")
+			if err := http.ListenAndServe(":80", acmeMgr.ChallengeHandler(acmeCfg.Domain)); !errors.Is(err, http.ErrServerClosed) { //nolint:gosec
+				logger.Error("acme http server failed", "error", err)
+			}
+		}()
+		logger.Info("scutum API starting (HTTPS/ACME)", "addr", port, "domain", acmeCfg.Domain)
+		go func() {
+			ln, err := tls.Listen("tcp", port, acmeMgr.TLSConfig())
+			if err != nil {
+				logger.Fatal("acme tls listener failed", "error", err)
+			}
+			if err := server.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
+				logger.Fatal("https server failed", "error", err)
+			}
+		}()
+	} else if useTLS {
 		logger.Info("scutum API starting (HTTPS)", "addr", port, "cert", certFile)
 		go func() {
 			if err := server.ListenAndServeTLS(certFile, keyFile); !errors.Is(err, http.ErrServerClosed) {
--- go.mod
+++ go.mod
@@ -19,6 +19,8 @@ require (
 	github.com/prometheus/common v0.66.1 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	golang.org/x/net v0.52.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 )
